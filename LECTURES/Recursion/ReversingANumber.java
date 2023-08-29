a1c80d451a535f7026a8b7e78e39695fe3afa15b
CodeJeet2005LIKHITH <codejeetlikhith2005@gmail.com>
2023-08-29 16:09:15 +0530

    commit

--- LECTURES/Recursion/ReversingANumber.java
+++ LECTURES/Recursion/ReversingANumber.java
@@ -8,7 +8,20 @@ public class ReversingANumber {
     public static void main(String[] args) {
         
         System.out.println(Reverse_one(1234));
+        System.out.println(Reverse_Perfect(812));
 
+    }
+    private static int Reverse_Perfect(int number) {
+        int length = (int)(Math.log10(number))+1;
+        return helper(number,length);
+        
+    }
+    private static int helper(int number, int length) {
+        if(number % 10 == number){
+            return number;
+        }
+        int rem =  number%10;
+        return rem*(int)Math.pow(10, length-1)+helper(number/10, length-1);
     }
     private static int Reverse_one(int i) {
         if(i == 0){
@@ -18,6 +31,8 @@ public class ReversingANumber {
         reversed_number = reversed_number*10+rem;
         return Reverse_one(i/10);
     }
+    // Now passing through Arguments
+
     
     
 }
