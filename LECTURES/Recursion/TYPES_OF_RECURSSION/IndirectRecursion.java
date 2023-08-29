2697b1c84669f9fcf8da306cfba1c2f97d7a427c
CodeJeet2005LIKHITH <codejeetlikhith2005@gmail.com>
2023-08-29 13:16:21 +0530

    IndirectRecursion

--- LECTURES/Recursion/TYPES_OF_RECURSSION/IndirectRecursion.java
+++ LECTURES/Recursion/TYPES_OF_RECURSSION/IndirectRecursion.java
@@ -1,6 +1,23 @@
 package LECTURES.Recursion.TYPES_OF_RECURSSION;
 
 public class IndirectRecursion {
-    // 
+    /*Indirect Recursion
+    a function(let say fun) is called indirect recursive if it calls 
+    another funtion(let say fun2) adn the fun2 calls fun directly or 
+    indirectly
+    Structure of the function
+
+    func(){
+        // some code
+        func2();
+        // some code
+    }
+    func2(){
+        // some code
+        func();
+        // some code
+    }
+
+     */
     
 }
