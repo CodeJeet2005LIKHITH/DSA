5a7e7a30e355c44322382b699f84568f4c37c27a
CodeJeet2005LIKHITH <codejeetlikhith2005@gmail.com>
2023-08-29 16:24:30 +0530

    CountZeros

--- LECTURES/Recursion/CountNoOfZeros.java
+++ LECTURES/Recursion/CountNoOfZeros.java
@@ -1,5 +1,19 @@
 package LECTURES.Recursion;
 
 public class CountNoOfZeros {
-    
+   public static void main(String[] args) {
+    int countZeros = CountZeros(15203501,0);
+    System.out.println(countZeros);
+   }
+
+    private static int CountZeros(int number,int count) {
+        if(number == 0){
+            return count;
+        }
+        if(number % 10 == 0){
+            return CountZeros(number/10, ++count);
+        }
+        return CountZeros(number/10, count);
+    }
+
 }
