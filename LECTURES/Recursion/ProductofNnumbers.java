b0a98c8df42ac9f78b0fef1178d9725e0243d1f4
CodeJeet2005LIKHITH <codejeetlikhith2005@gmail.com>
2023-08-29 11:49:21 +0530

    commit

--- LECTURES/Recursion/ProductofNnumbers.java
+++ LECTURES/Recursion/ProductofNnumbers.java
@@ -1,5 +1,17 @@
 package LECTURES.Recursion;
+// Factorial
 
 public class ProductofNnumbers {
+    public static void main(String[] args) {
+        int n=5;
+       int ans= Product(n);
+       System.out.println(ans);
+    }
+    static int Product(int n){
+        if(n - 1 == 0){
+            return n;
+        }
+        return n*Product(n-1);
+    }
     
 }
