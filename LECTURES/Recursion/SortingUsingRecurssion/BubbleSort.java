883f12d0156a37738530342a7e16dcd11d5b37b8
CodeJeet2005LIKHITH <codejeetlikhith2005@gmail.com>
2023-08-30 08:29:08 +0530

    commit

--- LECTURES/Recursion/SortingUsingRecurssion/BubbleSort.java
+++ LECTURES/Recursion/SortingUsingRecurssion/BubbleSort.java
@@ -1,13 +1,34 @@
-//package LECTURES.Recursion.SortingUsingRecurssion;
-//
-//public class BubbleSort {
-//    public static void main(String[] args) {
-//        int[]arr = {5,4,3,2,1};
-//        Bubble_Sort(arr);
-//    }
-//
-//    private static int[] Bubble_Sort(int[] arr) {
-//
-//    }
-//
-//}
+package LECTURES.Recursion.SortingUsingRecurssion;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+   public static void main(String[] args) {
+        int[]arr = {5,4,3,2,1};
+       Bubble_Sort(arr,0,arr.length-1);   
+       System.out.println(Arrays.toString(arr));
+     }
+
+   private static void Bubble_Sort(int[] arr,int start,int end) {
+    // Pattern same as Printing star pattern
+    if(end == 0){
+        return;
+    }
+    if(end > start){
+        if(arr[start] > arr[start+1]){
+            int temp = arr[start];
+            arr[start] = arr[start+1];
+            arr[start+1] = temp;
+            Bubble_Sort(arr, ++start, end);
+        }
+        else{
+            Bubble_Sort(arr, start+1, end);
+        }
+    }
+    else{
+        Bubble_Sort(arr, 0, --end);
+    }
+    
+   }
+
+}
