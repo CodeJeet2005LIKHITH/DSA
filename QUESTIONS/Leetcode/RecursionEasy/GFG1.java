0c4be6dbafa42fc10d94767eaf4e8395c639062a
CodeJeet2005LIKHITH <codejeetlikhith2005@gmail.com>
2023-08-30 10:43:14 +0530

    gfg#KunalKushwaha_Recrssion_assignment

---
+++ QUESTIONS/Leetcode/RecursionEasy/GFG1.java
@@ -0,0 +1,32 @@
+package QUESTIONS.Leetcode.RecursionEasy;
+// [Sum Triangle from Array](https://www.geeksforgeeks.org/sum-triangle-from-array/) `GFG`
+
+import java.util.Arrays;
+
+public class GFG1 {
+    public static void main(String[] args) {
+        int[]arr = {1,2,3,4,5};
+        int[]array = new int[arr.length-1];
+        Sum(arr,0,arr.length,array);
+    }
+    private static void Sum(int[]arr,int start,int end,int[]array){
+        if(arr.length == 1){
+            return;
+        }
+
+        if(start < end-1){
+            int x= arr[start]+arr[start+1];
+            
+            array[start] = x;
+          
+            Sum(arr, ++start, end,array);
+        }
+        else{
+            System.out.println(Arrays.toString(array));
+            Sum(array, 0, array.length,new int[array.length-1]);
+        }
+    }
+
+    
+    
+}
