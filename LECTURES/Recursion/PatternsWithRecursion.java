f5d9bb2cfcfb4136368a6763cf0517c684e04fbc
CodeJeet2005LIKHITH <codejeetlikhith2005@gmail.com>
2023-08-30 07:19:30 +0530

    c

--- LECTURES/Recursion/PatternsWithRecursion.java
+++ LECTURES/Recursion/PatternsWithRecursion.java
@@ -3,6 +3,7 @@ package LECTURES.Recursion;
 public class PatternsWithRecursion {
     public static void main(String[] args) {
         Triangle(5,0);
+        Triangle2(5,0);
     }
     private static void Triangle(int row,int colum){
         if(row == 0){
@@ -15,6 +16,19 @@ public class PatternsWithRecursion {
         else{
             System.out.println();
             Triangle(row-1, 0);
+        }
+    }
+    private static void Triangle2(int row, int colum){
+        if(row == 0){
+            return;
+        }
+        if(colum < row){
+            Triangle2(row, colum+1);
+            System.out.print("*");
+        }
+        else{
+            Triangle2(--row, 0);
+            System.out.println();
         }
     }
     
