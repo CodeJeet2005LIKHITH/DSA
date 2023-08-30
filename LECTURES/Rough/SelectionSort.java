877a4aba3b714050d6f6eedbcf394dca792d96b8
CodeJeet2005LIKHITH <codejeetlikhith2005@gmail.com>
2023-08-30 09:59:45 +0530

    cc

--- LECTURES/Rough/SelectionSort.java
+++ LECTURES/Rough/SelectionSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class SelectionSort {
     public static void main(String[] args) {
-        int[]arr = {4,3,2,1};
+        int[]arr = {1, 9, 81, 18, 63, 27, 45, 36, 54, 72, 90};
         selection(arr,arr.length,0,0);
         System.out.println(Arrays.toString(arr));
     }
