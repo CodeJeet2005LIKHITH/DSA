97d73a64d526627d6bf6492a417c8787274d12ec
CodeJeet2005LIKHITH <codejeetlikhith2005@gmail.com>
2023-09-03 09:55:07 +0530

    ChangesMadeInMergeSort

---
+++ LECTURES/Recursion/SortingUsingRecurssion/MergeSort.java
@@ -0,0 +1,53 @@
+package LECTURES.Recursion.SortingUsingRecurssion;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MergeSort {
+    public static void main(String[] args) {
+        int[]arr = {5,4,3,2,1};
+        Ms(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+        
+
+    }
+    private static void mergesort(int[]arr,int middle,int start,int end){
+        ArrayList<Integer> temp = new ArrayList<>();
+        int left = start;
+        int right = middle+1;
+        while(left <= middle && right <= end){
+            if(arr[left] > arr[right]){
+                temp.add(arr[right]);
+                right++;
+            }
+            else{
+                temp.add(arr[left]);
+                left++;
+            }
+        }
+        while(left <= middle){
+            temp.add(arr[left]);
+            left++;
+        }
+        while(right <= end){
+            temp.add(arr[right]);
+            right++;
+        }
+        for(int i=start;i<= end;i++){
+            arr[i] = temp.get(i-start);
+        }
+        
+    }
+    private static void Ms(int[]arr,int start,int end){
+        if(start >= end){
+            return;
+        }
+        int middle = start+(end-start)/2;
+
+        Ms(arr, start, middle);
+        Ms(arr, middle+1, end);
+        mergesort(arr,middle,start,end);
+    }
+    
+    
+}
