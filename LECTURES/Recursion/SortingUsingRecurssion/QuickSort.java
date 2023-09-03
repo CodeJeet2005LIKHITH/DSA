4e31993633d2b6857d684a3ddaab17e83c3a80b6
CodeJeet2005LIKHITH <codejeetlikhith2005@gmail.com>
2023-09-03 13:36:28 +0530

    QuickSort

---
+++ LECTURES/Recursion/SortingUsingRecurssion/QuickSort.java
@@ -0,0 +1,40 @@
+package LECTURES.Recursion.SortingUsingRecurssion;
+import java.util.Arrays;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        Quick_Sort(arr, 0, arr.length - 1);
+        System.out.println(Arrays.toString(arr));
+    }
+    private static void Quick_Sort(int[] arr, int start, int end) {
+        if(start >= end){
+            return;
+        }
+        int s=start;
+        int e = end;
+        int middle = start+(end-start)/2;
+        int pivot = arr[middle];
+        while(s <= e){
+            while(arr[s] < pivot){
+                ++s;
+            }
+            while(arr[e] > pivot){
+                --e;
+            }
+            if(s <= e){
+                int temp = arr[s];
+                arr[s] = arr[e];
+                arr[e] = temp;
+                ++s;
+                --e;
+
+            }
+
+        }
+        Quick_Sort(arr, start, pivot-1);
+        Quick_Sort(arr, pivot+1, end);
+
+    
+}
+}
