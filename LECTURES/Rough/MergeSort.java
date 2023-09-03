bcd9561b253e92574adc2f4c7901000a4839e506
CodeJeet2005LIKHITH <codejeetlikhith2005@gmail.com>
2023-09-03 14:43:15 +0530

    dingdong

--- LECTURES/Rough/MergeSort.java
+++ LECTURES/Rough/MergeSort.java
@@ -1,21 +1,28 @@
-package LECTURES.Recursion.SortingUsingRecurssion;
+package LECTURES.Rough;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 
 public class MergeSort {
     public static void main(String[] args) {
-        int[]arr = {3,1,4,2,3};
-        Ms(arr,0,arr.length-1);
+        int []arr = {5,4,3,2,1};
+        Merge_Sort(arr,0,arr.length-1);
         System.out.println(Arrays.toString(arr));
-        
-
     }
-    private static void mergesort(int[]arr,int middle,int start,int end){
+    private static void Merge_Sort(int[]arr,int start,int end){
+        if(start >= end){
+            return;
+        }
+        int middle = start+(end-start)/2;
+        Merge_Sort(arr, start, middle);
+        Merge_Sort(arr, middle+1, end);
+        Merge(arr, start,middle, end);
+    }
+    private static void Merge(int[]arr,int start,int middle,int end){
         ArrayList<Integer> temp = new ArrayList<>();
         int left = start;
         int right = middle+1;
-        while(left <= middle && right <= end){
+        while(left<= middle && right <= end){
             if(arr[left] > arr[right]){
                 temp.add(arr[right]);
                 right++;
@@ -27,27 +34,17 @@ public class MergeSort {
         }
         while(left <= middle){
             temp.add(arr[left]);
-            left++;
+            ++left;
         }
         while(right <= end){
             temp.add(arr[right]);
-            right++;
+            ++right;
         }
-        for(int i=start;i<= end;i++){
+        for(int i=start; i <= end;i++){
             arr[i] = temp.get(i-start);
-        }
-        
-    }
-    private static void Ms(int[]arr,int start,int end){
-        if(start >= end){
-            return;
-        }
-        int middle = start+(end-start)/2;
 
-        Ms(arr, start, middle);
-        Ms(arr, middle+1, end);
-        mergesort(arr,middle,start,end);
+        }
     }
-    
+
     
 }
