2e79bc045b1300ffbfba3255cf9a0a19fbfda09e
CodeJeet2005LIKHITH <codejeetlikhith2005@gmail.com>
2023-09-06 23:16:21 +0530

    commmit

--- LECTURES/Recursion/SubStringProblems.java
+++ LECTURES/Recursion/SubStringProblems.java
@@ -1,5 +1,6 @@
 package LECTURES.Recursion;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class SubStringProblems {
@@ -7,7 +8,10 @@ public class SubStringProblems {
         ArrayList<String> list = new ArrayList<>();
         String str = "LIKHITH";
         substrings(str,"",list);
-        System.out.println(list);
+//        substringswithoutListInArgs(str,"");
+        System.out.println(substringswithoutListInArgs(str,""));
+//        System.out.println(list);
+        System.out.println(substringAscii(str,""));
 
     };
     private static void substrings(String str, String newString,ArrayList<String> list){
@@ -17,6 +21,33 @@ public class SubStringProblems {
         }
         substrings(str.substring(1),newString+str.charAt(0),list);
         substrings(str.substring(1),newString,list);
+    }
+    private static ArrayList<String> substringswithoutListInArgs(String str,String newString){
+
+        if(str.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(newString);
+            return list;
+        }
+        ArrayList<String> left = substringswithoutListInArgs(str.substring(1),newString+str.charAt(0));
+        ArrayList<String> right = substringswithoutListInArgs(str.substring(1),newString);
+        left.addAll(right);
+        return left;
+    }
+    private static ArrayList<String> substringAscii(String str,String new_string){
+        if(str.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(new_string);
+            return list;
+        }
+        ArrayList<String> left = substringAscii(str.substring(1),new_string+str.charAt(0));
+        ArrayList<String> middleAscii = substringAscii(str.substring(1),new_string+0);
+        ArrayList<String>  right = substringAscii(str.substring(1),new_string);
+        left.addAll(middleAscii);
+        left.addAll(right);
+        return  left;
+
+
     }
 
 }
