6fd1bfe3f11d819d6e4892fac62cecbe8b3d7f6b
CodeJeet2005LIKHITH <codejeetlikhith2005@gmail.com>
2023-09-10 09:29:11 +0530

    commitKaroSaalo

--- LECTURES/Rough/RotatedBinarySearch.java
+++ LECTURES/Rough/RotatedBinarySearch.java
@@ -2,14 +2,22 @@ package LECTURES.Rough;
 
 public class RotatedBinarySearch {
     public static void main(String[] args) {
-        int[]arr=  {5,4,3,1,2};
-        RotatedbinarySearch(arr,0,arr.length-1,4);
+        int[]arr=  {5,6,7,4,3,2,1};
+        
+        System.out.println(RotatedbinarySearch(arr,0,arr.length-1,40));
 
     }
     private static int RotatedbinarySearch(int[]arr,int start,int end,int target){
         int middle = start+(end-start)/2;
+
+        if(start > end){
+            return -1;
+        }
+        if(arr[middle] == target){
+            return middle;
+        }
         if(arr[middle] > start){
-            if(arr[start] <= target && arr[middle] <= target){
+            if(arr[start] <= target && target <= arr[middle]){
                 return RotatedbinarySearch(arr, start, middle-1, target);
             }
             else{
@@ -17,7 +25,12 @@ public class RotatedBinarySearch {
             }
         }
         else{
-            if(arr[middle] <=)
+            if(arr[middle] <= target &&  target <= arr[end]){
+                return RotatedbinarySearch(arr, middle+1, end, target);
+            }
+            else{
+                return RotatedbinarySearch(arr, start, middle-1, target);
+            }
         }
     }
 }
